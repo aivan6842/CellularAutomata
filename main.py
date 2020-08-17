46bbf4e8e0ce85d1691478b18a8f3291bce8dd77
ALexei Ivanov <alex.ivanov0615@gmail.com>
2020-08-17 10:10:00 -0400

    working cellular automata!

--- main.py
+++ main.py
@@ -47,7 +47,6 @@ def toBinaryArray(num):
 def getRuleSet(num):
 
     arr = toBinaryArray(num)
-    print(arr)
     ruleSet = {}
     j=7
     for i in range(len(arr)):
@@ -57,13 +56,33 @@ def getRuleSet(num):
 
     return ruleSet
 
+def updateBoard(ruleSet, board):
+    for i in range(1, len(board)):
+        for j in range(len(board[i])):
+            board[i][j].state = getNextState(ruleSet, board, i, j)
+
+def getNextState(ruleSet, board, i, j):
+    if j == 0:
+        leftNeighbor = 0
+    else:
+        leftNeighbor = board[i-1][j-1].getState()
+    if j == len(board[0])-1:
+        rightNeighbor = 0
+    else:
+        rightNeighbor = board[i-1][j+1].getState()
+    topNeighbor = board[i-1][j].getState()
+
+
+    return ruleSet[tuple([leftNeighbor, topNeighbor, rightNeighbor])]
+
 def main():
     screen = pygame.display.set_mode([1214, 602])
     done = False
     clock = pygame.time.Clock()
 
     board = create_board()
-    ruleSet = getRuleSet(30)
+    ruleSet = getRuleSet(90)
+    updateBoard(ruleSet, board)
 
     while not done:
         for event in pygame.event.get():
